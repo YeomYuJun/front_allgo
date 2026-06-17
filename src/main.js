0fb27c50c0dbccad678c901c972260e5647a5287
YeomYuJun <yyj2046@gmail.com>
2026-06-17 21:21:41 +0900

    chore: remove vuetify dependency

--- src/main.js
+++ src/main.js
@@ -2,26 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import './assets/css/tokens.css'
 import App from './App.vue'
-import router from './router' // 라우터 임포트
-
-// Vuetify 관련 임포트
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import router from './router'
 import './assets/css/fonts.css'
-
-// Material Design Icons 임포트
-import '@mdi/font/css/materialdesignicons.css'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
 
 const app = createApp(App)
 
-app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
