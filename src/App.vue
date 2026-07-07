94e0fe51ad0cda6fa9c65834eae8befec05783ec
YeomYuJun <yyj2046@gmail.com>
2026-07-07 15:36:53 +0900

    refactor: replace vite default base styles with token-aligned reset (feedback 2)

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,3 @@
 <script setup>
 import MainLayout from './layout/MainLayout.vue';
 </script>
-
-<style>
-html {
-  overflow-y: auto !important;
-}
-
-header {
-  margin-bottom: 30px;
-  text-align: center;
-}
-
-h1 {
-  color: #333;
-}
-</style>
